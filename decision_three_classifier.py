6050eaba9425412458a996f8ea48e531347c7c35
Theesh <theekshana.s@ayra.com.au>
2020-05-13 22:35:36 +0530

    working script

--- decision_three_classifier.py
+++ decision_three_classifier.py
@@ -1,7 +1,76 @@
+# improting required librearies
+from matplotlib.colors import ListedColormap
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score
+import matplotlib.pyplot as plt
 
-print('it worked')
+sc = StandardScaler()
+
+# loading dataset
+dataset = pd.read_csv('./Python/Social_Network_Ads.csv')
+
+# creating X and y data
+X = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, 4].values
+
+# creating training and testing data
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, random_state=0, test_size=0.2)
+
+
+# feature scaling
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# creating the classifier
+classifier = DecisionTreeClassifier(random_state=0, criterion='entropy')
+
+# fitting data to the classifer
+classifier.fit(X_train, y_train)
+
+# predictions
+y_pred = classifier.predict(X_test)
+
+metrix = confusion_matrix(y_test, y_pred)
+print(metrix)
+
+acc = accuracy_score(y_test, y_pred)
+print(acc)
+
+# Visualising the Training set results
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.01),
+                     np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha=0.75, cmap=ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c=ListedColormap(('red', 'green'))(i), label=j)
+plt.title('Decision Tree Classification (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.01),
+                     np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha=0.75, cmap=ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c=ListedColormap(('red', 'green'))(i), label=j)
+plt.title('Decision Tree Classification (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
