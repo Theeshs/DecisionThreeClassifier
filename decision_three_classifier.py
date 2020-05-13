60e42c37d52d931d61c6cea0a594839438f5b32a
Theesh <theekshana.s@ayra.com.au>
2020-05-13 22:06:17 +0530

    changes

---
+++ decision_three_classifier.py
@@ -0,0 +1,7 @@
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+
+print('it worked')
